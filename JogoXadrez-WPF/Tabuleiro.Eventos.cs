a05e03188056b17885e4effdd087add6e1761c77
ThiagoServulo <thiago.servulo@sga.pucminas.br>
2022-09-02 12:28:50 -0300

    Criação da classe Tabuleiro.Eventos

---
+++ JogoXadrez-WPF/Tabuleiro.Eventos.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoXadrez_WPF
+{
+    partial class Tabuleiro
+    {
+        private void PictureBoxA1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 0));
+        }
+
+        private void PictureBoxA2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 1));
+        }
+
+        private void PictureBoxA3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 2));
+        }
+
+        private void PictureBoxA4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 3));
+        }
+
+        private void PictureBoxA5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 4));
+        }
+
+        private void PictureBoxA6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 5));
+        }
+
+        private void PictureBoxA7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 6));
+        }
+
+        private void PictureBoxA8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(0, 7));
+        }
+
+        private void PictureBoxB1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 0));
+        }
+
+        private void PictureBoxB2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 1));
+        }
+
+        private void PictureBoxB3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 2));
+        }
+
+        private void PictureBoxB4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 3));
+        }
+
+        private void PictureBoxB5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 4));
+        }
+
+        private void PictureBoxB6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 5));
+        }
+
+        private void PictureBoxB7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 6));
+        }
+
+        private void PictureBoxB8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(1, 7));
+        }
+
+        private void PictureBoxC1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 0));
+        }
+
+        private void PictureBoxC2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 1));
+        }
+
+        private void PictureBoxC3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 2));
+        }
+
+        private void PictureBoxC4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 3));
+        }
+
+        private void PictureBoxC5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 4));
+        }
+
+        private void PictureBoxC6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 5));
+        }
+
+        private void PictureBoxC7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 6));
+        }
+
+        private void PictureBoxC8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(2, 7));
+        }
+
+        private void PictureBoxD1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 0));
+        }
+
+        private void PictureBoxD2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 1));
+        }
+
+        private void PictureBoxD3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 2));
+        }
+
+        private void PictureBoxD4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 3));
+        }
+
+        private void PictureBoxD5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 4));
+        }
+
+        private void PictureBoxD6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 5));
+        }
+
+        private void PictureBoxD7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 6));
+        }
+
+        private void PictureBoxD8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(3, 7));
+        }
+
+        private void PictureBoxE1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 0));
+        }
+
+        private void PictureBoxE2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 1));
+        }
+
+        private void PictureBoxE3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 2));
+        }
+
+        private void PictureBoxE4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 3));
+        }
+
+        private void PictureBoxE5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 4));
+        }
+
+        private void PictureBoxE6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 5));
+        }
+
+        private void PictureBoxE7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 6));
+        }
+
+        private void PictureBoxE8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(4, 7));
+        }
+
+        private void PictureBoxF1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 0));
+        }
+
+        private void PictureBoxF2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 1));
+        }
+
+        private void PictureBoxF3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 2));
+        }
+
+        private void PictureBoxF4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 3));
+        }
+
+        private void PictureBoxF5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 4));
+        }
+
+        private void PictureBoxF6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 5));
+        }
+
+        private void PictureBoxF7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 6));
+        }
+
+        private void PictureBoxF8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(5, 7));
+        }
+
+        private void PictureBoxG1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 0));
+        }
+
+        private void PictureBoxG2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 1));
+        }
+
+        private void PictureBoxG3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 2));
+        }
+
+        private void PictureBoxG4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 3));
+        }
+
+        private void PictureBoxG5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 4));
+        }
+
+        private void PictureBoxG6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 5));
+        }
+
+        private void PictureBoxG7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 6));
+        }
+
+        private void PictureBoxG8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(6, 7));
+        }
+
+        private void PictureBoxH1Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 0));
+        }
+
+        private void PictureBoxH2Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 1));
+        }
+
+        private void PictureBoxH3Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 2));
+        }
+
+        private void PictureBoxH4Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 3));
+        }
+
+        private void PictureBoxH5Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 4));
+        }
+
+        private void PictureBoxH6Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 5));
+        }
+
+        private void PictureBoxH7Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 6));
+        }
+
+        private void PictureBoxH8Click(object sender, System.EventArgs e)
+        {
+            ProcessaPictureBoxClick(new Posicao(7, 7));
+        }
+    }
+}
--- JogoXadrez-WPF/Tabuleiro.cs
+++ JogoXadrez-WPF/Tabuleiro.cs
@@ -10,15 +10,17 @@ namespace JogoXadrez_WPF
         public int Colunas = 8;
         private Peca[,] _pecasEmJogo;
         private PictureBox[,] _pictureBoxes;
-        private HashSet<Peca> _pecasCapturadas;
         private Posicao _origem;
         private Cor _jogadorAtual;
         private int _turno;
+        private int _quantidadeBrancasCapturadas;
+        private int _quantidadePretasCapturadas;
 
         public Tabuleiro()
         {
+            _quantidadeBrancasCapturadas = 0;
+            _quantidadePretasCapturadas = 0;
             _turno = 1;
-            _pecasCapturadas = new HashSet<Peca>();
             _jogadorAtual = Cor.Branco;
             _origem = null;
             InitializeComponent();
@@ -33,15 +35,13 @@ namespace JogoXadrez_WPF
                 { pictureBoxG1, pictureBoxG2, pictureBoxG3, pictureBoxG4, pictureBoxG5, pictureBoxG6, pictureBoxG7, pictureBoxG8 },
                 { pictureBoxH1, pictureBoxH2, pictureBoxH3, pictureBoxH4, pictureBoxH5, pictureBoxH6, pictureBoxH7, pictureBoxH8 }
             } ;
-            AtualizaLabelJogador();
-            AtualizaLabelTurno();
+            AtualizaLabels();
             InicializaTabuleiro();
         }
 
         private void MudaJogador()
         {
             _jogadorAtual = _jogadorAtual == Cor.Branco ? Cor.Preto : Cor.Branco;
-            AtualizaLabelJogador();
         }
 
         public Peca AcessarPeca(int linha, int coluna)
@@ -81,10 +81,21 @@ namespace JogoXadrez_WPF
             labelTurno.Text = $"Turno: {_turno}";
         }
 
+        private void AtualizaLabelsPecasCapturadas()
+        {
+            labelPecasCapturadasBranco.Text = $"Peças capturadas: {_quantidadePretasCapturadas}";
+            labelPecasCapturadasPreto.Text = $"Peças capturadas: {_quantidadeBrancasCapturadas}";
+        }
+
+        private void AtualizaLabels()
+        {
+            AtualizaLabelJogador();
+            AtualizaLabelTurno();
+            AtualizaLabelsPecasCapturadas();
+        }
         private void IncrementaTurno()
         {
             _turno++;
-            AtualizaLabelTurno();
         }
 
         private void MostrarTabuleiro()
@@ -172,6 +183,8 @@ namespace JogoXadrez_WPF
                 MostrarTabuleiro();
                 ExecutaMovimento(_origem, posicao);
                 MudaJogador();
+                IncrementaTurno();
+                AtualizaLabels();
                 _origem = null;
             }
         }
@@ -184,331 +197,17 @@ namespace JogoXadrez_WPF
             ColocarPeca(peca, destino);
             if (pecaCapturada != null)
             {
-                _pecasCapturadas.Add(pecaCapturada);
+                if(pecaCapturada.CorDaPeca == Cor.Branco)
+                {
+                    _quantidadeBrancasCapturadas++;
+                }
+                else
+                {
+                    _quantidadePretasCapturadas++;
+                }
             }
             _pictureBoxes[origem.Linha, origem.Coluna].Image = null;
-            IncrementaTurno();
             return pecaCapturada;
-        }
-
-        private void PictureBoxA1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 0));
-        }
-
-        private void PictureBoxA2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 1));
-        }
-
-        private void PictureBoxA3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 2));
-        }
-
-        private void PictureBoxA4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 3));
-        }
-
-        private void PictureBoxA5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 4));
-        }
-
-        private void PictureBoxA6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 5));
-        }
-
-        private void PictureBoxA7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 6));
-        }
-
-        private void PictureBoxA8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(0, 7));
-        }
-
-        private void PictureBoxB1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 0));
-        }
-
-        private void PictureBoxB2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 1));
-        }
-
-        private void PictureBoxB3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 2));
-        }
-
-        private void PictureBoxB4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 3));
-        }
-
-        private void PictureBoxB5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 4));
-        }
-
-        private void PictureBoxB6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 5));
-        }
-
-        private void PictureBoxB7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 6));
-        }
-
-        private void PictureBoxB8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(1, 7));
-        }
-
-        private void PictureBoxC1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 0));
-        }
-
-        private void PictureBoxC2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 1));
-        }
-
-        private void PictureBoxC3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 2));
-        }
-
-        private void PictureBoxC4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 3));
-        }
-
-        private void PictureBoxC5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 4));
-        }
-
-        private void PictureBoxC6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 5));
-        }
-
-        private void PictureBoxC7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 6));
-        }
-
-        private void PictureBoxC8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(2, 7));
-        }
-
-        private void PictureBoxD1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 0));
-        }
-
-        private void PictureBoxD2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 1));
-        }
-
-        private void PictureBoxD3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 2));
-        }
-
-        private void PictureBoxD4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 3));
-        }
-
-        private void PictureBoxD5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 4));
-        }
-
-        private void PictureBoxD6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 5));
-        }
-
-        private void PictureBoxD7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 6));
-        }
-
-        private void PictureBoxD8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(3, 7));
-        }
-
-        private void PictureBoxE1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 0));
-        }
-
-        private void PictureBoxE2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 1));
-        }
-
-        private void PictureBoxE3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 2));
-        }
-
-        private void PictureBoxE4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 3));
-        }
-
-        private void PictureBoxE5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 4));
-        }
-
-        private void PictureBoxE6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 5));
-        }
-
-        private void PictureBoxE7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 6));
-        }
-
-        private void PictureBoxE8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(4, 7));
-        }
-
-        private void PictureBoxF1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 0));
-        }
-
-        private void PictureBoxF2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 1));
-        }
-
-        private void PictureBoxF3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 2));
-        }
-
-        private void PictureBoxF4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 3));
-        }
-
-        private void PictureBoxF5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 4));
-        }
-
-        private void PictureBoxF6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 5));
-        }
-
-        private void PictureBoxF7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 6));
-        }
-
-        private void PictureBoxF8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(5, 7));
-        }
-
-        private void PictureBoxG1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 0));
-        }
-
-        private void PictureBoxG2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 1));
-        }
-
-        private void PictureBoxG3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 2));
-        }
-
-        private void PictureBoxG4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 3));
-        }
-
-        private void PictureBoxG5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 4));
-        }
-
-        private void PictureBoxG6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 5));
-        }
-
-        private void PictureBoxG7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 6));
-        }
-
-        private void PictureBoxG8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(6, 7));
-        }
-
-        private void PictureBoxH1Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 0));
-        }
-
-        private void PictureBoxH2Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 1));
-        }
-
-        private void PictureBoxH3Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 2));
-        }
-
-        private void PictureBoxH4Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 3));
-        }
-
-        private void PictureBoxH5Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 4));
-        }
-
-        private void PictureBoxH6Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 5));
-        }
-
-        private void PictureBoxH7Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 6));
-        }
-
-        private void PictureBoxH8Click(object sender, System.EventArgs e)
-        {
-            ProcessaPictureBoxClick(new Posicao(7, 7));
         }
     }
 }
